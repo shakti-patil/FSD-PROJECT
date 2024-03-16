7a0f2bbf9db68d31f5c42764454b969ac5e5ead2
shakti-patil <161614314+shakti-patil@users.noreply.github.com>
2024-03-16 19:05:54 +0530

    index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const product = [
     {
         id: 0,
-        image: '../2.jpg',
+        image: './2.jpg',
         title: 'Sanheiser HeadSet',
         price: 120,
     },
     {
         id: 1,
-        image: '../5.jpeg',
+        image: './5.jpg',
         title: 'Air Pods Pro',
         price: 60,
     },
     {
         id: 2,
-        image: '../1.jpeg',
+        image: './1.jpg',
         title: 'Sumsung 23 Ultra',
         price: 799,
     },
     {
         id: 3,
-        image: '../4.jpeg',
+        image: './4.jpg',
         title: 'iPhone 12',
         price: 989,
     }
